20eba03462220a9dfb6bf869cc6c6fc8dd021724
Lynn <lynn@foldr.moe>
2019-04-07 05:27:24 +0200

    whatever, it's 4MB

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ a cutesy stenography theory that takes half an hour to learn!
 try it [here](http://lynn.github.io/sonnet)
 
 # build instructions
-* download https://www.kilgarriff.co.uk/BNClists/all.num.o5 and put it here.
 * run `npm i` then `npm run build`.
-* open the output `dist/index.html` in your browser.
+* open the output file `dist/index.html` in your browser.
--- prebuild.js
+++ prebuild.js
@@ -6,15 +6,8 @@ fs.copyFileSync("src/index.html", "dist/index.html");
 
 if (fs.existsSync("src/wordlist.js")) {
   console.log("wordlist.js already generated. That's good.");
-  console.log("You can delete all.num.o5 now, if you'd like.");
 } else {
   console.log("Generating wordlist.js...");
-  if (!fs.existsSync("all.num.o5")) {
-    console.log("Oops! I need a file called `all.num.o5` in this directory.");
-    console.log("Please fetch it from: https://www.kilgarriff.co.uk/BNClists/all.num.o5");
-    console.log("Read more about this frequency list here: https://www.kilgarriff.co.uk/bnc-readme.html");
-    process.exit(1);
-  };
   var contents = fs.readFileSync("all.num.o5", "utf8");
   var wordlist = "export default `\n" + contents + "\n`.trim().split('\\n')";
   fs.writeFileSync("src/wordlist.js", wordlist);
